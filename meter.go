b3467fcf07dcd8285803203fdc7d9103c80a7906
Mathieu Payeur Levallois <math.pay@gmail.com>
2014-08-05 23:03:39 -0400

    Better resolution on ewma values.

--- meter.go
+++ meter.go
@@ -123,9 +123,9 @@ func (m *StandardMeter) GetKeys(ct time.Time, name string, currentTime bool) []s
 		m.CrunchEWMA(ct)
 		keys = make([]string, 4)
 
-		keys[1] = fmt.Sprintf(name, "rate._1min", t, fmt.Sprintf("%.4f", m.Rate1()))
-		keys[2] = fmt.Sprintf(name, "rate._5min", t, fmt.Sprintf("%.4f", m.Rate5()))
-		keys[3] = fmt.Sprintf(name, "rate._15min", t, fmt.Sprintf("%.4f", m.Rate15()))
+		keys[1] = fmt.Sprintf(name, "rate._1min", t, fmt.Sprintf("%.6f", m.Rate1()))
+		keys[2] = fmt.Sprintf(name, "rate._5min", t, fmt.Sprintf("%.6f", m.Rate5()))
+		keys[3] = fmt.Sprintf(name, "rate._15min", t, fmt.Sprintf("%.6f", m.Rate15()))
 	} else {
 		keys = make([]string, 1)
 	}
